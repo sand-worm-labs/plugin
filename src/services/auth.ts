7e797a3129d26a28d4b73d8d12ddf61edf91d818
Angry Coding <mailidanielifechukwu@gmail.com>
2025-06-15 15:18:43 +0000

    chore: services

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -6,20 +6,23 @@ import {
 } from "@elizaos/core";
 
 export class AuthService extends Service {
-    static serviceType: ServiceType = ServiceType.TRANSCRIPTION;
+    stop(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    static serviceType = ServiceType.TRANSCRIPTION;
     capabilityDescription: string = "This is the auuth service for sandworm.";
      initialize(runtime: IAgentRuntime): Promise<void> {
       return null;
     }
 
-    async login(address: string): Promise<{ token: string }> {
-        const response = await fetch("https://api.sandworm.dev/login", {
-            method: "POST",
-            body: JSON.stringify({ address }),
-            headers: { "Content-Type": "application/json" },
-        });
+    // async login(address: string): Promise<{ token: string }> {
+    //     const response = await fetch("https://api.sandworm.dev/login", {
+    //         method: "POST",
+    //         body: JSON.stringify({ address }),
+    //         headers: { "Content-Type": "application/json" },
+    //     });
 
-        if (!response.ok) throw new Error("Login failed");
-       // return await response.json();
-    }
+    //     if (!response.ok) throw new Error("Login failed");
+    //    // return await response.json();
+    // }
 }
--- src/services/chart.ts
+++ src/services/chart.ts
@@ -6,8 +6,13 @@ import {
 } from "@elizaos/core";
 
 export class ChartService extends Service {
+    stop(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    
     capabilityDescription: string ="This is the query service for sandworm.";
-    static serviceType: ServiceType = ServiceType.TRANSCRIPTION;
+    static serviceType = ServiceType.TRANSCRIPTION;
 
      initialize(runtime: IAgentRuntime): Promise<void> {
       return null;
